80b646795e27f489d303b635ecaf0750b2986544
Yanir Aton <yanirahahah2007@gmail.com>
2025-09-06 11:10:55 +0300

    Initial Commit

---
+++ Deficit/DeficitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeficitApp.swift
+//  Deficit
+//
+//  Created by Yanir Aton on 06/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct DeficitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeficitTests/DeficitTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeficitTests.swift
+//  DeficitTests
+//
+//  Created by Yanir Aton on 06/09/2025.
+//
+
+import Testing
+@testable import Deficit
+
+struct DeficitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
